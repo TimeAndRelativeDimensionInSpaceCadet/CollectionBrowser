a93efff4955c7e902c35fe8fc5c4d3f623e4d0b0
Erik White <ewhite@nvisia.com>
2025-01-08 11:00:16 -0600

    add default headers as constant

--- src/Util/apiUtil.js
+++ src/Util/apiUtil.js
@@ -2,14 +2,19 @@ export const BASE_URL = 'https://api.discogs.com';
 
 const createUrl = (base, path) => `${base}${path}`;
 
+export const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+  Authorization:
+    'Discogs key=axDUlhdXQPElPALxAoEU, secret=xARcGhyrIjMhGpjNIKmCvHknIHPoNiFp',
+};
+
 export const makeCollectionRequest = () => [
-  createUrl(BASE_URL, '/users/jabbrowocky/collection/folders/0/releases?sort=artist'),
+  createUrl(
+    BASE_URL,
+    '/users/jabbrowocky/collection/folders/0/releases?sort=artist'
+  ),
   {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization:
-        'Discogs key=axDUlhdXQPElPALxAoEU, secret=xARcGhyrIjMhGpjNIKmCvHknIHPoNiFp',
-    },
+    headers: DEFAULT_HEADERS,
   },
 ];
