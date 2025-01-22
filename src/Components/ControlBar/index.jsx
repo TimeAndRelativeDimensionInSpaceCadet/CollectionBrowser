0e67908fc8028686ec867c6e31280b8e71010ea0
Erik White <ewhite@nvisia.com>
2025-01-22 16:29:46 -0600

    memoize class concatenation

--- src/Components/ControlBar/index.jsx
+++ src/Components/ControlBar/index.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export const ControlBar = ({ className, onSearch = () => {} }) => {
-  const classes = (className ?? '').concat(
-    ' sticky w-full h-20 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10'
+  const classes = useMemo(
+    () =>
+      'sticky w-full h-20 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10'
+        .concat(` ${className ?? ''}`)
+        .trim(),
+    [className]
   );
+  
   return <div className={classes}></div>;
 };
