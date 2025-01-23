c1cc71f34586af3b54738b4155f72d3e41f2d3f1
Erik White <ewhite@nvisia.com>
2025-01-23 17:06:34 -0600

    update toolbar to use flex alignment

--- src/Components/ControlBar/index.jsx
+++ src/Components/ControlBar/index.jsx
@@ -4,7 +4,7 @@ import { DebouncedInput } from '../DebouncedInput';
 export const ControlBar = ({ className, onSearch }) => {
   const classes = useMemo(
     () =>
-      'sticky w-full h-20 p-2 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10'
+      'sticky flex flex-row-reverse items-center w-full h-20 p-2 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10'
         .concat(` ${className ?? ''}`)
         .trim(),
     [className]
@@ -17,16 +17,10 @@ export const ControlBar = ({ className, onSearch }) => {
     [onSearch]
   );
 
-  /* const handleSearch = e => {
-    if (e.key === 'Enter') {
-      onSearch(searchText);
-    }
-  }; */
-
   return (
     <div className={classes}>
       {onSearch && typeof onSearch == 'function' && (
-        <DebouncedInput handleChange={handleSearch} />
+        <DebouncedInput placeholder="Search" handleChange={handleSearch} />
       )}
     </div>
   );
