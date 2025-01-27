f014b55c51d36e123f9f92e3b8fcb65b10ea8792
Erik White <ewhite@nvisia.com>
2025-01-27 15:54:49 -0600

    more refactoring on collection list.

--- src/Components/CollectionList/index.jsx
+++ src/Components/CollectionList/index.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 import { useCollection } from '../../Hooks/useCollection';
 import { useCollectionSearch } from '../../Hooks/useCollectionSearch';
 import { CollectionItem } from '../CollectionItem';
@@ -12,13 +12,11 @@ export const CollectionList = () => {
   const { searchResults, handleSearchResults } =
     useCollectionSearch(collection);
 
-  const handleCollectionItemMap = () => {
-    const toUse = searchResults ? searchResults : collection;
-
-    return toUse?.map((e, i) => (
+  const handleCollectionItemMap = toUse => {
+    return toUse?.map(({ basic_information }) => (
       <CollectionItem
-        key={i}
-        itemInfo={e.basic_information}
+        key={basic_information.title}
+        itemInfo={basic_information}
         containerRef={collectionContainer}
       />
     ));
@@ -27,17 +25,16 @@ export const CollectionList = () => {
   return (
     <>
       {collection && !error && !loading && (
-        <div
-          className="size-full overflow-y-auto overflow-x-hidden"
-          //onScroll={handleScroll}
-        >
+        <div className="size-full overflow-y-auto overflow-x-hidden">
           <div className="pb-3">
             <ControlBar onSearch={handleSearchResults} />
             <div
               className="size-full grid grid-cols-auto-fit-250 gap-3 pt-3 px-3"
               ref={collectionContainer}
             >
-              {handleCollectionItemMap()}
+              {handleCollectionItemMap(
+                searchResults ? searchResults : collection
+              )}
             </div>
           </div>
         </div>
