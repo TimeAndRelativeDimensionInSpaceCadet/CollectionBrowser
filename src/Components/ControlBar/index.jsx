23a129f4c022d680acd399c9c4747ad0a69a276a
Erik White <ewhite@nvisia.com>
2025-01-27 14:28:18 -0600

    adjust padding in control bar to align with padding in grid

--- src/Components/ControlBar/index.jsx
+++ src/Components/ControlBar/index.jsx
@@ -4,7 +4,7 @@ import { DebouncedInput } from '../DebouncedInput';
 
 export const ControlBar = ({ className, onSearch }) => {
   const classes = useClassConcat(
-    'sticky flex flex-row-reverse w-full h-20 p-2 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10',
+    'sticky flex flex-row-reverse w-full h-20 p-3 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10',
     className
   );
 
