a8ff6dbfb7d94b4f55c41a6f0662fa4478799d59
Erik White <ewhite@nvisia.com>
2025-02-06 10:19:33 -0600

    trigger deploy test

--- src/Components/SortBySelectInput/index.jsx
+++ src/Components/SortBySelectInput/index.jsx
@@ -65,9 +65,7 @@ export const SortBySelectInput = ({
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className={`${
-              !open ? '' : 'transform-flip'
-            } size-6 transition-all`.trim()}
+            className={`${!open ? '' : 'transform-flip '}size-6 transition-all`}
           >
             <path
               strokeLinecap="round"
