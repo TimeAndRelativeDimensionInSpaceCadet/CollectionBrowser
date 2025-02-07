99e4401fd192fe8676ad3779fe31aacc27a69978
Erik White <ewhite@nvisia.com>
2025-02-07 15:55:15 -0600

    mad scientist reduce function to generate controls

--- src/Components/ControlBar/index.jsx
+++ src/Components/ControlBar/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useState, useMemo } from 'react';
 import { useClassConcat } from '../../Hooks/useClassConcat';
 import { SortToggleButton } from '../SortToggleButton';
 import { DebouncedInput } from '../DebouncedInput';
@@ -12,6 +12,7 @@ export const ControlBar = ({
   onSortByChange,
 }) => {
   const { current: sortOptions } = useRef(['artist', 'title', 'year']);
+  const [drawerOpen, setDrawerOpen] = useState(true);
   const classes = useClassConcat(
     'sticky flex flex-row-reverse max-w-full w-full h-20 p-3 top-0 left-0 bg-slate-700 shadow-md shadow-slate-700/80 z-10',
     className
@@ -38,54 +39,47 @@ export const ControlBar = ({
     [onSortByChange]
   );
 
+  const controls = useMemo(() => {
+    const result = [
+      onSortDirectionChange && typeof onSortDirectionChange === 'function' && (
+        <SortToggleButton
+          key="SortDirectionToggle"
+          className="self-center"
+          handleSortDirectionChange={handleSortChange}
+        />
+      ),
+      onSortByChange && typeof onSortByChange === 'function' && (
+        <SortBySelectInput
+          key="SortByInput"
+          className="self-center mr-2"
+          sortOptions={sortOptions}
+          onSortByChange={handleSortByChange}
+        />
+      ),
+      onSearch && typeof onSearch === 'function' && (
+        <DebouncedInput
+          key="SearchInput"
+          className="mr-2 self-center"
+          placeholder="Search"
+          handleChange={handleSearch}
+        />
+      ),
+    ].reduce((a, b) => (b ? [...a, b] : a), []);
+
+    return result;
+  }, [onSortByChange, onSortDirectionChange, onSearch]);
+
+  const handleToggleDrawer = () => setDrawerOpen(prev => !prev);
+
   return (
     <>
-      <SidebarDrawer open={true}>
-        {onSortDirectionChange &&
-          typeof onSortDirectionChange === 'function' && (
-            <SortToggleButton
-              className="self-center"
-              handleSortDirectionChange={handleSortChange}
-            />
-          )}
-        {onSortByChange && typeof onSortByChange === 'function' && (
-          <SortBySelectInput
-            className="self-center mr-2"
-            sortOptions={sortOptions}
-            onSortByChange={handleSortByChange}
-          />
-        )}
-        {onSearch && typeof onSearch === 'function' && (
-          <DebouncedInput
-            className="mr-2 self-center"
-            placeholder="Search"
-            handleChange={handleSearch}
-          />
-        )}
+      <SidebarDrawer open={drawerOpen}>
+        <button className="bg-transparent" onClick={handleToggleDrawer}>
+          x
+        </button>
+        {controls}
       </SidebarDrawer>
-      <div className={classes}>
-        {onSortDirectionChange &&
-          typeof onSortDirectionChange === 'function' && (
-            <SortToggleButton
-              className="self-center"
-              handleSortDirectionChange={handleSortChange}
-            />
-          )}
-        {onSortByChange && typeof onSortByChange === 'function' && (
-          <SortBySelectInput
-            className="self-center mr-2"
-            sortOptions={sortOptions}
-            onSortByChange={handleSortByChange}
-          />
-        )}
-        {onSearch && typeof onSearch === 'function' && (
-          <DebouncedInput
-            className="mr-2 self-center"
-            placeholder="Search"
-            handleChange={handleSearch}
-          />
-        )}
-      </div>
+      <div className={classes}>{controls}</div>
     </>
   );
 };
