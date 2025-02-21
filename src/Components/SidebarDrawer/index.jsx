bdfee02e14e903f0329a403937bfea7956c8a953
Erik White <ewhite@nvisia.com>
2025-02-21 11:21:39 -0600

    remove complex js and state from drawer, simplify to use simple memoized class concatenation hook and separate layers using absolute positioning and fragment so that drawer animation was no longer contingent on visibility of overlay

--- src/Components/SidebarDrawer/index.jsx
+++ src/Components/SidebarDrawer/index.jsx
@@ -1,36 +1,27 @@
 import { useClassConcat } from '../../Hooks/useClassConcat';
-import { useEffect, useState } from 'react';
 
 export const SidebarDrawer = ({
   children,
   open,
   onBackDropClick = () => {},
 }) => {
-  const [overlayOpen, setOverlayOpen] = useState(false);
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
   const overlayClasses = useClassConcat(
-    overlayOpen ? 'visible' : 'hidden',
-    'absolute top-0 left-0 size-full backdrop-blur-sm z-20'
+    'absolute top-0 left-0 size-full backdrop-blur-sm z-10',
+    open ? 'visible' : 'hidden'
   );
-
   const drawerClasses = useClassConcat(
-    drawerOpen ? 'w-[65%]' : 'w-0',
-    'absolute transition-all right-0 h-full bg-slate-700 shadow-[rgba(0,0,0,0.5)_-3px_0px_6px_0px]'
+    'absolute transition-all right-0 top-0 h-full bg-slate-700 shadow-[rgba(0,0,0,0.5)_-3px_0px_6px_0px] z-20',
+    open ? ' w-[65%]' : ' w-0'
   );
-
-  useEffect(() => {
-    setTimeout(() => setOverlayOpen(open), open ? 0 : 150);
-    setTimeout(() => setDrawerOpen(open), open ? 25 : 0);
-  }, [open]);
 
   return (
-    <div className={overlayClasses} onClick={onBackDropClick}>
+    <>
+      <div className={overlayClasses} onClick={onBackDropClick} />
       <div className={drawerClasses}>
         <div className="p-3 size-full flex flex-col items-end overflow-hidden">
           {children}
         </div>
       </div>
-    </div>
+    </>
   );
 };
